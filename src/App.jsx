2964cce06a55b7a113aa7032e4a49027c54df652
James Upson <upsonjames1@gmail.com>
2025-08-09 01:27:54 +0100

    update v1.3
    just buttons and new effects

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import jsLogo from './assets/js.svg';
 import cssLogo from './assets/css.svg';
 import htmlLogo from './assets/html.svg';
 import TextType from './TextType';
+import MagicBento from './MagicBento';
+import TiltedCard from './TiltedCard';
 
 function App() {
   return (
@@ -12,13 +14,15 @@ function App() {
       <div className={styles.darkVeilOverlay}></div>
       <div className={styles.container}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '2rem' }}>
-          <TextType
-            text={["Welcome to my portfolio"]}
-            typingSpeed={75}
-            pauseDuration={1500}
-            showCursor={true}
-            cursorCharacter="|"
-          />
+          <h1 className={styles.aboutMe} style={{ background: 'none', boxShadow: 'none', marginBottom: 0, padding: 0 }}>
+            <TextType
+              text={["Welcome to my portfolio"]}
+              typingSpeed={75}
+              pauseDuration={1500}
+              showCursor={true}
+              cursorCharacter="|"
+            />
+          </h1>
         </div>
         <section className={styles.aboutMe}>
           <h1>About Me</h1>
@@ -27,21 +31,100 @@ function App() {
           </p>
           <button className={styles.aboutMeButton}>Learn More</button>
         </section>
+        <div style={{ marginTop: '2.5rem' }}>
+          <MagicBento
+            textAutoHide={true}
+            enableStars={true}
+            enableSpotlight={true}
+            enableBorderGlow={true}
+            enableTilt={true}
+            enableMagnetism={true}
+            clickEffect={true}
+            spotlightRadius={300}
+            particleCount={12}
+            glowColor="132, 0, 255"
+          />
+        </div>
+        <div style={{ margin: '2.5rem auto', display: 'flex', justifyContent: 'center' }}>
+          <TiltedCard
+            imageSrc="https://i.scdn.co/image/ab67616d0000b273d9985092cd88bffd97653b58"
+            altText="Kendrick Lamar - GNX Album Cover"
+            captionText="Kendrick Lamar - GNX"
+            containerHeight="300px"
+            containerWidth="300px"
+            imageHeight="300px"
+            imageWidth="300px"
+            rotateAmplitude={12}
+            scaleOnHover={1.2}
+            showMobileWarning={false}
+            showTooltip={true}
+            displayOverlayContent={true}
+            overlayContent={
+              <p className="tilted-card-demo-text">
+                Kendrick Lamar - GNX
+              </p>
+            }
+          />
+        </div>
         <section>
           <div className={styles.languagesTitle}>Languages I Know</div>
           <div className={styles.languages}>
-            <div className={styles.languageIcon} title="JavaScript">
-              <img src={jsLogo} alt="JavaScript" width="40" height="40" />
-            </div>
-            <div className={styles.languageIcon} title="React">
-              <img src={reactLogo} alt="React" width="40" height="40" />
-            </div>
-            <div className={styles.languageIcon} title="CSS">
-              <img src={cssLogo} alt="CSS" width="40" height="40" />
-            </div>
-            <div className={styles.languageIcon} title="HTML">
-              <img src={htmlLogo} alt="HTML" width="40" height="40" />
-            </div>
+            <TiltedCard
+              imageSrc={jsLogo}
+              altText="JavaScript"
+              captionText="JavaScript"
+              containerHeight="64px"
+              containerWidth="64px"
+              imageHeight="64px"
+              imageWidth="64px"
+              rotateAmplitude={12}
+              scaleOnHover={1.18}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={false}
+            />
+            <TiltedCard
+              imageSrc={reactLogo}
+              altText="React"
+              captionText="React"
+              containerHeight="64px"
+              containerWidth="64px"
+              imageHeight="64px"
+              imageWidth="64px"
+              rotateAmplitude={12}
+              scaleOnHover={1.18}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={false}
+            />
+            <TiltedCard
+              imageSrc={cssLogo}
+              altText="CSS"
+              captionText="CSS"
+              containerHeight="64px"
+              containerWidth="64px"
+              imageHeight="64px"
+              imageWidth="64px"
+              rotateAmplitude={12}
+              scaleOnHover={1.18}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={false}
+            />
+            <TiltedCard
+              imageSrc={htmlLogo}
+              altText="HTML"
+              captionText="HTML"
+              containerHeight="64px"
+              containerWidth="64px"
+              imageHeight="64px"
+              imageWidth="64px"
+              rotateAmplitude={12}
+              scaleOnHover={1.18}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={false}
+            />
           </div>
         </section>
       </div>
